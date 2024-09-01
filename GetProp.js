8336848116fa2c06b85f9e39a83f9c7a880e8e35
RajeshKumar200061 <dhimanraj457@gmail.com>
2024-09-01 12:40:26 +0530

    first commit

---
+++ GetProp.js
@@ -0,0 +1,13 @@
+let sidebar = document.querySelector(".sidebar");
+let body = document.querySelector("#body");
+let tran =document.querySelector(".hamburgar")
+function showsidebar() {
+  sidebar.style.display = "flex";
+
+}
+
+
+function hidesidebar() {
+  sidebar.style.display = "none";
+  
+}
